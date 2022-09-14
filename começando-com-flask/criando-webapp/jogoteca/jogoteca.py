dab17b50f285cb24737203230e64b4f2c144b10b
Andrewgaabriel <andrew.gabrielgomes@gmail.com>
2022-09-14 11:40:08 -0300

    feat: add form para criar um novo jogo + usa 'post'

--- começando-com-flask/criando-webapp/jogoteca/jogoteca.py
+++ começando-com-flask/criando-webapp/jogoteca/jogoteca.py
@@ -1,23 +1,43 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 
-class jogo:
+class Jogo:
     def __init__(self, nome, categoria, console):
         self.nome = nome
         self.categoria = categoria
         self.console = console
 
 
+jogo1 = Jogo('Tetris', 'Puzzle', 'Atari')
+jogo2 = Jogo('God Of War', 'Rack n Slash', 'PS2')
+jogo3 = Jogo('Mortal Kombat', 'Luta', 'PS2')    
+
+jogosLista = [jogo1, jogo2, jogo3]
+
 app = Flask(__name__)
 
-@app.route('/inicio')
-def inicio():
-    jogo1 = jogo('Tetris', 'Puzzle', 'Atari')
-    jogo2 = jogo('God Of War', 'Rack n Slash', 'PS2')
-    jogo3 = jogo('Mortal Kombat', 'Luta', 'PS2')    
-    
-    jogosLista = [jogo1, jogo2, jogo3]
+
+
+@app.route('/')
+def index():
     return render_template('lista.html', titulo='Jogos', jogos=jogosLista)
+
+
+@app.route('/novo')
+def novo():    
+    return render_template('novo.html', titulo='Novo Jogo')
+
+
+@app.route('/criar', methods=['POST',])
+def criar():
+    nome = request.form['nome']
+    categoria = request.form['categoria']
+    console = request.form['console']
+    jogo = Jogo(nome, categoria, console)
+    jogosLista.append(jogo)
+    
+    return render_template('lista.html', titulo='Jogos', jogos=jogosLista)
+
 
 
 app.run(host='0.0.0.0', port=8080, debug=True) 
